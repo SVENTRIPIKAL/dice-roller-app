18cb6199e4d886379de02e8b8456c2d2bff5a725
Jose Garza <90730468+SVENTRIPIKAL@users.noreply.github.com>
2023-06-17 12:07:29 -0500

    -initial commit -first jetpack app -very basic app with interactive button to roll a dice

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val mainActivityView: LinearLayout = binding.root
+        val diceRollButton: Button = binding.rollButton
+        val diceImageView: ImageView = binding.actionImageVector
+
+        diceRollButton.setText(R.string.letsRoll)
+        diceRollButton.setOnClickListener {
+            rollDice(diceImageView)
+        }
+
+        setContentView(mainActivityView)
+    }
+
+    private fun rollDice(diceImageView: ImageView) {
+
+        val imageResource = when (
+            Random.nextInt(1, 7)
+        ) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImageView.setImageResource(imageResource)
+    }
+}
